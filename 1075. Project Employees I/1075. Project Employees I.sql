194815e8bc902e2ae7598e600d15e119ba7a6355
Pratik <pdfulkar@yahoo.com>
2025-04-26 22:39:24 +0530

    new commit

---
+++ 1075. Project Employees I/1075. Project Employees I.sql
@@ -0,0 +1,5 @@
+select  p.project_id, round(avg(experience_years),2) as average_years
+from employee e
+left join project p 
+on e.employee_id = p.employee_id
+group by project_id
--- 570. Managers with at Least 5 Direct Reports/leetcode.py
+++ 570. Managers with at Least 5 Direct Reports/leetcode.py
@@ -1,9 +1,16 @@
-n = 7
-bina = []
-for i in range(n+1):
-     bina.append(str(bin(i)))
-print(bina)
-for j in bina:
-    print(j)
-    for k in range(len(j)):
-        print(j[::])
+
+# set1= set(s)
+# if len(s) % 2 == 0:
+#     for n in range(len(s)//2):   
+#         if s[:n+1:1] == s[-n-1::1]:
+#             print(True) 
+
+s = "abcabcabcabc"
+# sol = False
+print(len(s) % 2 == 0)
+# print(s[(len(s)/2)::1])
+# if len(s) % 2 == 0:   
+#     if s[:(len(s)/2):1] == s[-(len(s)/2)::1]:
+#         print(s[:(len(s)/2):1] , s[(len(s)/2)::1])
+        # sol == True
+# print(sol)
