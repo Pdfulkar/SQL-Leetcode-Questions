d82d5991b3f5dba1750e2934e63e73557e22e0a0
Pratik <pdfulkar@yahoo.com>
2025-04-27 18:00:10 +0530

    new update

--- 1075. Project Employees I/1075. Project Employees I.sql
+++ 1075. Project Employees I/1075. Project Employees I.sql
@@ -2,4 +2,5 @@ select  p.project_id, round(avg(experience_years),2) as average_years
 from employee e
 left join project p 
 on e.employee_id = p.employee_id
+where project_id is NOT NULL
 group by project_id
