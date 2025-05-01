1baaff36cab0a5734a8d60caf3fa52ae6969c80f
Pratik <pdfulkar@yahoo.com>
2025-05-01 23:30:20 +0530

    new update

--- 329.py
+++ 329.py
@@ -1,15 +1,7 @@
-class Solution:
-    def findTheDifference(self, s: str, t: str) -> str:
-        a = list(s)
-        b = list(t)
-        d = []
-        for i in a:
-                if i in b:
-                    d.append(i)
-        for p in d:
-            if p in b:
-                b.remove(p)
-        if len(b) > 1 or len(b) == 0:
-            return 0 
-        else:
-            return b[0]
+n = 9
+numberarr = []
+for i in range(1,n):
+    numberarr.append(i)
+if len(numberarr) > 0 :
+    numberarr.remove([n for n in numberarr j])
+print(numberarr)
