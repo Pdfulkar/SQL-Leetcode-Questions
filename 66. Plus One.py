544e9cdb460a8c58abb817818b2ca443f2fc6c4f
Pratik <pdfulkar@yahoo.com>
2025-05-15 22:20:07 +0530

    New update

---
+++ 66. Plus One.py
@@ -0,0 +1,5 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = int(''.join(map(str, digits)))
+        number += 1
+        return [int(digit) for digit in str(number)]
